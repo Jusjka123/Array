62952c37241e08dfbf8ffd280ca394027dffa84b
Jusjka123 <juliana.jusjka@gmail.com>
2024-02-25 19:23:16 +0200

    small fix

--- src/test/java/First.java
+++ src/test/java/First.java
@@ -4,27 +4,25 @@ class ArrayChecker {
 
     public static void main(String[] args) {
         int[] array = {4, 5, -3, 15, -2, 19, 6, -2, -4, 20, -17};
-        System.out.println("Given array: "+ Arrays.toString(array));
+        System.out.println("Given array: " + Arrays.toString(array));
 
         int x = 0;
         int y = 0;
         int z = 0;
         int w = 0;
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] < 0) {
+        for (int number : array) {
+            if (number < 0) {
                 x++;
-            } else if (array[i] > 0) {
+            } else if (number > 0) {
                 y++;
             }
-            if (array[i] % 2 != 0) {
+
+            if (number % 2 != 0) {
                 z++;
-            } else if (array[i] % 2 == 0) {
+            } else {
                 w++;
             }
         }
         System.out.println("Array has " + x + " negative, " + y + " positive, " + z + " odd and " + w + " even numbers.");
     }
-
-
-
 }
